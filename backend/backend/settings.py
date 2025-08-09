17d6f8993eecbe0290cc05ad5ca5ed4ee88492a6
TahamidurTaief <tahamidurtaief@gmail.com>
2025-08-09 10:59:32 +0600

    config settings for producttion

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -179,14 +180,11 @@ USE_TZ = True
 
 
 
-
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'mediafiles'
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 
 
